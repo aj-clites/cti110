19fed277360d9c7474ad90f206de28f43358108f
aj-clites <160262594+aj-clites@users.noreply.github.com>
2024-02-16 11:36:22 -0500

    Add files via upload

---
+++ P1LAB1_ClitesAndrew.py
@@ -0,0 +1,12 @@
+# Andrew Clites
+
+# 2024-02-16
+
+# P1LAB1
+
+# A program that will accept a user input and output their name with a welcome message.
+
+name = input('Your name here...')
+
+print('Hey', name)
+print('Welcome to ZyBooks!')
