625e8708409bffae60cdbccdbe168fe52b853498
aj-clites <160262594+aj-clites@users.noreply.github.com>
2024-05-12 17:11:54 -0400

    Uploaded P1HW1_ClitesAndrew
    Basic output program

---
+++ P1HW1_ClitesAndrew.py
@@ -0,0 +1,27 @@
+# Andrew Clites
+
+# 2024-02-20
+
+# P1HW1_ClitesAndrew
+
+# A program that will take an integer as an input and produce an output with the square and cube of the integer.
+
+int1 = int(input('Enter integer:\n'))
+
+print('You entered:', int1)
+
+intSquared = int1 ** 2
+
+intCubed = int1 ** 3
+
+print(int1, 'squared is', intSquared)
+print('And', int1, 'cubed is', intCubed)
+
+int2 = int(input('Enter another integer:\n'))
+
+sum1 = int1 + int2
+
+product = int1 * int2
+
+print(int1, '+', int2, 'is', sum1)
+print(int1, '*', int2, 'is', product)
